a29c5745b4d4bdf974f803f9b8414952ddb7d0a0
seharnisar <147860041+seharnisar@users.noreply.github.com>
2025-07-01 16:04:52 +0500

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Force Python 3.10 environment
-python3.10 -m venv venv
-source venv/bin/activate
-
-# Install dependencies with version constraints
-pip install pysqlite3-binary==0.5.2
-pip install chromadb==0.4.24 --no-deps
-pip install -r requirements.txt
